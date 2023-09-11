7be38416e1039ef6c150c83bcbd7751d506f9f19
Pablo Aylwin <p.aylwin.k@uc.cl>
2023-09-10 21:39:05 -0300

    esqueleto homepage

---
+++ E1/vuelta-segura/src/homepage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <header>
+        <h1>Bienvenido a Mi Aplicación</h1>
+      </header>
+      <main>
+        <p>Esta es la página principal de mi aplicación.</p>
+        <p>Puedes agregar contenido, enlaces y componentes aquí.</p>
+      </main>
+      <footer>
+        <p>Derechos de autor &copy; 2023 Mi Aplicación</p>
+      </footer>
+    </div>
+  );
+}
+
+export default HomePage;
