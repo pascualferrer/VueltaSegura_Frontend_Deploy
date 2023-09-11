cd641c69598c2f4a76c44bb013420e2e0bf5195b
Pablo Aylwin <p.aylwin.k@uc.cl>
2023-09-11 20:39:51 -0300

    barra de navegación y página principal agregadas

--- E1/vuelta-segura/src/homepage.jsx
+++ E1/vuelta-segura/src/homepage.jsx
@@ -1,18 +1,47 @@
 import React from 'react';
+import "./homepage.css"
+import NavBar from './navbar'
 
 function HomePage() {
   return (
-    <div className="home-page">
-      <header>
-        <h1>Bienvenido a Mi Aplicación</h1>
-      </header>
-      <main>
-        <p>Esta es la página principal de mi aplicación.</p>
-        <p>Puedes agregar contenido, enlaces y componentes aquí.</p>
-      </main>
-      <footer>
-        <p>Derechos de autor &copy; 2023 Mi Aplicación</p>
-      </footer>
+    <div className="pag">
+      <nav className="navbar">
+        <NavBar />
+      </nav>
+        <div className="home-page">
+            <header>
+                <h1>Bienvenido a Vuelta Segura</h1>
+            </header>
+            <div className="registro">
+                <div className='conductor'>
+                    <h2>Chofer</h2>
+                    <label htmlFor='user_chofer'>Usuario:</label>
+                    <li><input id="user_chofer" type="text" placeholder="Ingrese su nombre"></input></li>
+                    <br />
+                    <label htmlFor='password_chofer'>Contraseña:</label>
+                    <li><input id="password_chofer" type="password"placeholder="Ingrese su contraseña"></input></li>
+                    <br />
+                    <button className='boton_chofer'>
+                        Ingresar
+                    </button>
+                </div>
+                <div className='usuario'>
+                    <h2>Usuario</h2>
+                    <label htmlFor='user_usuario'>Usuario:</label>
+                    <li><input id="user_usuario" type="text" placeholder="Ingrese su nombre"></input></li>
+                    <br />
+                    <label htmlFor='password_usuario'>Contraseña:</label>
+                    <li><input id="password_usuario" type="password"placeholder="Ingrese su contraseña"></input></li>
+                    <br />
+                    <button className='boton_usuario'>
+                        Ingresar
+                    </button>
+                </div>
+            </div>
+            <footer>
+                <p>Derechos de autor &copy; 2023 Mi Aplicación</p>
+            </footer>
+        </div>
     </div>
   );
 }
