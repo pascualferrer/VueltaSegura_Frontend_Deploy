d785c6e34c617694e5f84fab7c4df5ffae4bbe8e
Pablo Aylwin <p.aylwin.k@uc.cl>
2023-09-10 21:46:36 -0300

    reset

--- E1/vuelta-segura/src/App.jsx
+++ E1/vuelta-segura/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import HomePage from './homepage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +9,6 @@ function App() {
   return (
     <>
       <div className = "App">
-        <HomePage />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
